d1770bdc79ec65233bc3ac98d32d8234f4c6021d
DmitrijLosev <dmitrijslosevs@inbox.lv>
2024-02-26 10:27:32 +0200

    first commit

---
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -0,0 +1,26 @@
+import {legacy_createStore} from "redux";
+
+const initState = {
+    themeId: 1,
+}
+
+
+
+
+export const themeReducer = (state:StateType = initState, action: ActionType): StateType => { // fix any
+    switch (action.type) {
+        // дописать
+        case "SET_THEME_ID":
+            return {...state, themeId: action.id}
+        default:
+            return state
+    }
+}
+
+export const changeThemeId = (id: number): ChangeThemeIdType => ({ type: 'SET_THEME_ID', id }) as const
+
+export const store = legacy_createStore(themeReducer)
+
+export type StateType = typeof initState
+type ActionType = ReturnType<typeof changeThemeId>
+type ChangeThemeIdType = { type: 'SET_THEME_ID', id:number }
